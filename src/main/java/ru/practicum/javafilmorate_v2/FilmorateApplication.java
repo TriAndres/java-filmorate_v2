4f051b118769ab9eb8a3b1b28b7ed65290797ece
Andrei <An.S-1@yandex.ru>
2024-11-25 09:16:53 +0300

    first commit

---
+++ src/main/java/ru/practicum/javafilmorate_v2/FilmorateApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.javafilmorate_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmorateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmorateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/practicum/javafilmorate_v2/JavaFilmorateV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.javafilmorate_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFilmorateV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
